7f99127e77af2028c2e3b7c33449eaa550344007
Han <mgu1228@gmail.com>
2020-03-13 02:37:59 +0900

    리사이클러뷰 추가 시도
    리사이클러뷰를 추가하려는 가상한 시도

--- app/src/main/java/com/example/idiomcollector/MainActivity.java
+++ app/src/main/java/com/example/idiomcollector/MainActivity.java
@@ -1,42 +1,64 @@
 package com.example.idiomcollector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.idiomcollector.databinding.ActivityMainBinding;
+
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    SharedPreferences sp;
-    LinearLayout linearLayout;
+    //Systems
+    SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+    ActivityMainBinding binding;
 
+    //Data Structure
     ArrayList<String> lst;
+
+    //Views
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setActivity(this);
 
-        sp = getPreferences(MODE_PRIVATE);
-        editor = sp.edit();
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        editor = sharedPreferences.edit();
 
-        linearLayout = findViewById(R.id.list_layout);
+        recyclerView = findViewById(R.id.list_layout);
 
-        lst = loadPreference();
-        initializeList();
+        ArrayList<String> lst = new ArrayList();
+
+        Map<String, ?> totalValue = sharedPreferences.getAll();
+
+        for(Map.Entry<String, ?> entry : totalValue.entrySet())
+        {
+            lst.add(entry.getValue().toString());
+        }
+
+        for(String str : lst)
+        {
+            TextView textView = new TextView(this);
+
+            textView.setText(str);
+            recyclerView.addView(textView);
+        }
     }
 
-    public void AddText(View view) {
+    public void onAddButtonClick(View view) {
         EditText editText = findViewById(R.id.edit_text);
 
         TextView textView = new TextView(this);
@@ -45,13 +67,10 @@ public class MainActivity extends AppCompatActivity {
 
         if(!str.isEmpty())
         {
-            addPreference(str);
-            updateList(str);
-
             editor.putString(str, str);
             textView.setText(str);
 
-            linearLayout.addView(textView);
+            recyclerView.addView(textView);
         }
         else
         {
@@ -59,38 +78,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public ArrayList<String> loadPreference()
+    public void onRemoveButtonClick(View view)
     {
-        ArrayList<String> lst = new ArrayList();
-
-        Map<String, ?> totalValue = sp.getAll();
-
-        for(Map.Entry<String, ?> entry : totalValue.entrySet())
-        {
-            lst.add(entry.getValue().toString());
-        }
-
-        return lst;
-    }
-
-    public void addPreference(String str)
-    {
-
-    }
-
-    public void initializeList()
-    {
-        for(String str : lst)
-        {
-            TextView textView = new TextView(this);
-
-            textView.setText(str);
-            linearLayout.addView(textView);
-        }
-    }
-
-    public void updateList(String str)
-    {
-
+        recyclerView.removeAllViews();
     }
 }
